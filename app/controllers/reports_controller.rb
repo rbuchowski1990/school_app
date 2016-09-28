cdebbcd6bf08f8b02fa70650d22a732b20dcc3a3
r.buchowski <r.buchowski1990@gmail.com>
2016-09-28 12:13:57 +0200

    added vistors index path

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,3 +1,7 @@
 class ReportsController < ApplicationController
   expose(:subject_items) { SubjectItem.includes(:teacher, :students) }
+
+
+  # def index
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
     get :subjects
   end
 
-  root 'students#index'
+  get :report_subjects, to: 'reports#subjects'
+
+  get :visitors, to: 'visitors#index'
+
+  root 'visitors#index'
 end
