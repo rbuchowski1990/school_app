a82783087e976af526309017698f4c219d34a56f
r.buchowski <r.buchowski1990@gmail.com>
2016-09-29 22:21:23 +0200

    added belongs to rel in subject model, handled avarage subject item notes

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,14 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = SubjectItemNote.where(:subject_item_id => subject_item, :student_id => object).pluck(:value)
+    notes = [0] if notes.empty?
+    avg = notes.inject(:+).to_f / notes.size
+    "%.2f" % avg
+  end
+
+  def all_notes(subject_item)
+    noteArr = SubjectItemNote.where(:subject_item_id => subject_item, :student_id => object).pluck(:value)
+    noteArr.map(&:inspect).join(', ')
   end
 end
