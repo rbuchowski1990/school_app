8b91395de3f3b9fd5a4f423d6a40723bac7045e4
r.buchowski <r.buchowski1990@gmail.com>
2016-10-02 15:42:34 +0200

    added students list in report subjects

--- app/decorators/subject_item_decorator.rb
+++ app/decorators/subject_item_decorator.rb
@@ -1,5 +1,10 @@
 class SubjectItemDecorator < BaseDecorator
-  def  title_with_assigned_teacher
+  def title_with_assigned_teacher
     teacher.present? ? "#{title} (#{teacher.decorate.full_name})" : title
+  end
+
+  def subject_students
+    students = object.students.pluck(:first_name, :last_name)
+    students.map! { |stud| stud.join(' ') }.join(', ')
   end
 end
